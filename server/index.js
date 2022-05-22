5ef28fc30009f97f3d69bb0c2ae8a00913810ebb
Aniket Kumar <aniketk28@gmail.com>
2022-05-22 15:16:21 +0530

    Mongoose connection, Post Model, addPost and getPost added

--- server/index.js
+++ server/index.js
@@ -3,3 +3,24 @@ import bodyParser from 'body-parser';
 import mongoose from "mongoose";
 import cors from 'cors';
 
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({limit: "30mb", extendend : true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extendend : true}));
+app.use(cors);
+
+const userName = "dukesocial";
+const userPass = "dukesocial12345";
+const CONNECTION_URL = `mongodb+srv://${userName}:${userPass}@cluster0.rybon.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .catch((error)=> console.log(error.message));
+app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+
+
+// mongoose.set('useFindAndModify', false);
