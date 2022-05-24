fd86057c0e3ece71ebaee89b565a9567209e4a54
Aniket Kumar <aniketk28@gmail.com>
2022-05-24 10:42:24 +0530

    Created User Model

---
+++ server/models/user.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    middleName: String,
+    lastName: String,
+    userId: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    dob: {
+        type: Date,
+        required: true,
+    },
+    bio: {
+        type: String,
+        required: true,
+    },
+
+    profilepic: String,
+    posts: [
+        {
+            post_id: String,
+        }
+    ],
+    followers: [
+        {
+            user_id: String,
+        }
+    ],
+    following: [
+        {
+            user_id: String,
+        }
+    ],
+});
+
+const User = mongoose.model('User', userSchema);
+export default User
