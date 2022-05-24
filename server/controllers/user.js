093d392e4976e0f3fe88c8c277e571727c1985bb
Aniket Kumar <aniketk28@gmail.com>
2022-05-24 18:29:56 +0530

    Update user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,9 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const createUser = async (req, res) => {
     try {
@@ -32,23 +36,25 @@ export const login = async (req, res) => {
     const body = req.body;
     const user = await User.findOne({email: body.email});
 
-    if (user) {
-        const validPassword = await bcrypt.compare(body.password, user.password);
-        if (validPassword) {
-            res.status(200).json(user);
-        }
-        else {
-            res.status(400).json({
-                success: false,
-                message: 'Invalid Password',
-            });
-        }
-    }
-    else {
+    if (!user) {
         res.status(400).json({
             success: false,
             message: 'User does not exists',
         });
     }
+    const validPassword = await bcrypt.compare(body.password, user.password);
+    if (!validPassword) {
+        res.status(400).json({
+            success: false,
+            message: 'Invalid Password',
+        });
+    }
+    
+    const accessToken = jsonwebtoken.sign({
+        firstName: user.firstName,
+        email: user.email,
+    }, process.env.ACCESS_TOKEN_SECRET);
+
+    res.status(200).json({ accessToken: accessToken });
 }
 
