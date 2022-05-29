fbe83e5711b3e52d778d416db635740528c3dbed
Aniket Kumar <aniketk28@gmail.com>
2022-05-29 09:29:01 +0530

    Update posts.js
    Updated posts routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,13 @@
 import express from "express";
 import postRoute from "./post.js";
-import { getPosts, createPost } from "../controllers/posts.js";
+import { getPosts, createPost, getFeed } from "../controllers/posts.js";
 import authenticateToken from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get('/', authenticateToken, getPosts);
 router.post('/', authenticateToken, createPost);
+router.get('/feed', authenticateToken, getFeed);
 
 router.use('/post', authenticateToken, postRoute);
 
