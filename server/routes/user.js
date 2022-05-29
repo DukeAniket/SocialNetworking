20a1709786a22803ce2e59399ab02b3841a974bf
Aniket Kumar <aniketk28@gmail.com>
2022-05-29 09:29:26 +0530

    Update user.js
    Updated user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createUser, getProfile, login } from "../controllers/user.js";
+import { createUser, follow, getFollowers, getFollowing, getProfile, login, unfollow } from "../controllers/user.js";
+import authenticateToken from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", login);
-router.get("/profile", getProfile);
+router.get("/profile", authenticateToken ,getProfile);
+router.get("/following", authenticateToken, getFollowing);
+router.get("/followers", authenticateToken, getFollowers);
+router.post('/follow', authenticateToken, follow);
+router.post('/unfollow', authenticateToken, unfollow);
 
 export default router;
