30a63ea5431773d84c7fc3848bf822444059f325
Aniket Kumar <aniketk28@gmail.com>
2022-05-29 09:26:27 +0530

    Update comments.js
    Added like comment, dislike comment;
    Updated get Comments and added Comments according to newly created postComment.js model

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import shallowEqual from "../middleware/shallowEqual.js";
+import PostComment from "../models/postComment.js";
 import PostMessage from "../models/postMessage.js";
 
 export const getComments = async (req, res) => {
@@ -10,7 +11,8 @@ export const getComments = async (req, res) => {
             res.status(404).json({message: "Posts Not Found"});
         }
         
-        const postComments = post.comments;
+        const postComments = await PostComment.findById(post.comments);
+
         res.status(200).json(postComments);
 
     } catch (error) {
@@ -22,6 +24,7 @@ export const addComment = async (req, res) => {
     const comment = req.body;
     comment.user = req.user.firstName;
     comment.user_email = req.user.email;
+    const newComment = new PostComment(comment);
     try {
         const post = await PostMessage.findById(req.query.id);
 
@@ -29,9 +32,9 @@ export const addComment = async (req, res) => {
             res.status(404).json({message: "Posts Not Found"});
         }
 
-        post.comments.push(comment);
+        post.comments.push(newComment);
         post.save();
-        res.status(201).json(comment);
+        res.status(201).json({success: true, message: "Comment Added!"});
 
     } catch (error) {
         res.status(500).json({message: error.message});
@@ -45,25 +48,42 @@ export const deleteComment = async (req, res) => {
     
     try {
         const post = await PostMessage.findById(post_id);
+        const comment = await PostComment.findById(comment_id);
+
+        if(comment.user_email!==req.user.email) {
+            return res.status(403).json({success: false, message: "Action Forbidden"})
+        }
+
+        post.comments.pull(comment);
+        await PostComment.deleteOne(comment);
         
-        var dc = -1;
-        var objID = mongoose.Types.ObjectId(comment_id.toString());
-        for (var i = 0; i < post.comments.length; i++ ) {
-            if (shallowEqual(post.comments[i]._id, objID)) {
-                if(post.comments[i].user_email!==req.user.email) {
-                    return res.status(403).json({success: false, message: "Action Forbidden"})
-                }
-                dc = i
-                break;
-            }
-        }
-        if (dc== -1) {
-            return res.status(404).json({success: false, message:"Comment Not Found!"})
-        }
-        const deletedComment = post.comments.splice(dc, 1);
         post.save();
-        res.status(200).json({success: true, message: "Comment Deleted Successfully!", deletedComment: deletedComment});
+        res.status(200).json({success: true, message: "Comment Deleted Successfully!"});
     } catch (error) {
         res.status(400).json({success: false, message: error.message});
+    }
+}
+
+export const likeComment = async (req, res) => {
+    try {
+        const liker = await User.find({email: req.user.email});
+        const liked = await PostComment.findById(req.body.comment_id);
+        liked.likes.push(liker);
+        liked.save();
+        res.status(200).json({success: true, message: "Comment Liked!"});
+    } catch (error) {
+        res.status(500).json({success: false, message: error.message});
+    }
+}
+
+export const unlikeComment = async (req, res) => {
+    try {
+        const liker = await User.find({email: req.user.email});
+        const liked = await PostComment.findById(req.body.comment_id);
+        liked.likes.pull(liker);
+        liked.save();
+        res.status(200).json({success: true, message: "Comment Unliked!"});
+    } catch (error) {
+        res.status(500).json({success: false, message: error.message});
     }
 }
